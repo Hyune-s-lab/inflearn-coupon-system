b72317b764ddc6362c484a2bd12d610ff696a506
Hyune-c <choi8608@gmail.com>
2023-08-12 02:28:00 +0900

    [쿠폰 Project] 프로젝트 세팅하기 - 문제점

--- api/src/test/kotlin/com/example/api/service/ApplyServiceTest.kt
+++ api/src/test/kotlin/com/example/api/service/ApplyServiceTest.kt
@@ -3,7 +3,13 @@ package com.example.api.service
 import com.example.api.repository.CouponRepository
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import org.springframework.boot.test.context.SpringBootTest
+import kotlin.system.measureTimeMillis
 
 @SpringBootTest
 class ApplyServiceTest(
@@ -14,4 +20,33 @@ class ApplyServiceTest(
         applyService.applyCoupon(1)
         couponRepository.count() shouldBe 1
     }
+
+    test("여러명 응모") {
+        var i = 0L
+
+        runBlocking {
+            withContext(Dispatchers.Default) {
+                massiveRun {
+                    applyService.applyCoupon(i++)
+                }
+            }
+        }
+
+        couponRepository.count() shouldBe 100
+    }
 })
+
+suspend fun massiveRun(action: suspend () -> Unit) {
+    val n = 1000 // 시작할 코루틴의 갯수
+    val k = 1 // 코루틴 내에서 반복할 횟수
+    val elapsed = measureTimeMillis {
+        coroutineScope { // scope for coroutines
+            repeat(n) {
+                launch {
+                    repeat(k) { action() }
+                }
+            }
+        }
+    }
+    println("### $elapsed ms 동안 ${n * k}개의 액션을 수행했습니다.")
+}
