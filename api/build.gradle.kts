23096c389b01549568dac6df8d28f25cde259ce0
Hyune-c <choi8608@gmail.com>
2023-08-20 00:36:45 +0900

    [Kafka를 활용하여 문제 해결하기] Producer 사용하기

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -8,6 +8,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.kafka:spring-kafka")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     runtimeOnly("com.mysql:mysql-connector-j")
--- api/src/main/kotlin/com/example/api/service/ApplyService.kt
+++ api/src/main/kotlin/com/example/api/service/ApplyService.kt
@@ -1,6 +1,7 @@
 package com.example.api.service
 
 import com.example.api.domain.Coupon
+import com.example.api.producer.CouponCreateProducer
 import com.example.api.repository.CouponCountRepository
 import com.example.api.repository.CouponRepository
 import org.springframework.stereotype.Service
@@ -8,13 +9,16 @@ import org.springframework.stereotype.Service
 @Service
 class ApplyService(
     private val couponRepository: CouponRepository,
-    private val couponCountRepository: CouponCountRepository
+    private val couponCountRepository: CouponCountRepository,
+
+    private val couponCreateProducer: CouponCreateProducer
 ) {
     fun applyCoupon(userId: Long) {
-        if (couponCountRepository.increment("couponCount") >= 100) {
+        if (couponCountRepository.increment("couponCount") > 100) {
             return
         }
 
         couponRepository.save(Coupon(userId = userId))
+        couponCreateProducer.create(userId)
     }
 }
