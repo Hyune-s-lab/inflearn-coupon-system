11a49d1731b1fd5a1dddac55c6737162760fd998
Hyune-c <choi8608@gmail.com>
2023-08-29 23:35:40 +0900

    [시스템 개선하기] 쿠폰을 발급하다가 에러가 발생하면 어떻게 하나요 ?

--- consumer/src/main/kotlin/com/example/consumer/consumer/CouponCreatedConsumer.kt
+++ consumer/src/main/kotlin/com/example/consumer/consumer/CouponCreatedConsumer.kt
@@ -1,16 +1,27 @@
 package com.example.consumer.consumer
 
 import com.example.consumer.domain.Coupon
+import com.example.consumer.domain.FailedEvent
 import com.example.consumer.repository.CouponRepository
+import com.example.consumer.repository.FailedEventRepository
+import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
 
 @Component
 class CouponCreatedConsumer(
     private val couponRepository: CouponRepository,
+    private val failedEventRepository: FailedEventRepository
 ) {
+    private val log = LoggerFactory.getLogger(this.javaClass)
+
     @KafkaListener(topics = ["coupon_create"], groupId = "group_1")
     fun listener(userId: Long) {
-        couponRepository.save(Coupon(userId = userId))
+        try {
+            couponRepository.save(Coupon(userId = userId))
+        } catch (e: Exception) {
+            log.error("Failed to save coupon: $userId")
+            failedEventRepository.save(FailedEvent(userId = userId))
+        }
     }
 }
